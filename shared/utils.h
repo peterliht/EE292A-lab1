8b238eca3f97d15e44229a9e2d20ce1bf0e0006b
Haitong Li <lihaitong.pku@gmail.com>
2018-04-25 00:31:29 -0700

    16/8bit kernels, utils.cpp, and main_16b.cpp

--- shared/utils.h
+++ shared/utils.h
@@ -2,7 +2,7 @@
 #define __UTILS_H_
 
 void write_weights_file(char *filename, float *weights, int num_weights);
-bool read_weights_file(char *filename, float *weights);
+bool read_weights_file(char *filename, short *weights);  //original: float *weights
 unsigned int convert_endian_4bytes(unsigned int input);
 int parse_MNIST_images(const char* file, unsigned char** X);
 int parse_MNIST_labels(const char* file, unsigned char** y);
